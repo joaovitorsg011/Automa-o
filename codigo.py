7c1af29df47816f073cee06dd7f78bb17b410e32
joaovitorsg011 <joaovitorsimao13ff@gmail.com>
2025-04-06 14:10:44 -0300

    Reformulando o codigo com funcionalidades

--- codigo.py
+++ codigo.py
@@ -1,41 +1,37 @@
-# Passo 1 = Entrar no sistema da empresa
-    #Link = https://dlp.hashtagtreinamentos.com/python/intensivao/login
-# Passo 2 = Fazer login
-# Passo 3 = Importar a base de dados
-# Passo 4 = Cadastrar um produto
-# Passo 5 = Repetir o passo 4 ate cadastrar todos produtos
-    
 import pyautogui
 from time import sleep
-# pyautogui.click - clicar com mouse
-# pyautogui.write - escrever texto
-# pyautogui.press - apertar 1 tecla
-# pyautogui.hotkey - combinacoes de teclas
-# pyautogui.scroll - rolar a tela
-pyautogui.PAUSE = 1.5
-pyautogui.press('win')
-pyautogui.write('chrome')
-pyautogui.press('enter')
-sleep(2.5)
-pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
-pyautogui.press('enter')
-sleep(3.5)
-pyautogui.press('tab')
-sleep(2.5)
-pyautogui.hotkey('ctrl', 'a')
-pyautogui.write('joaovitorsimao13ff@gmail.com')
-pyautogui.press(('tab'))
-pyautogui.write('30220130Oa?')
-pyautogui.press('tab')
-pyautogui.press('enter')
-sleep(3.5)
+import pandas as pd
 
+tabela = pd.read_csv('produtos.csv')
 
-import pandas
+def acessar_sistema():
+    pyautogui.press('win')
+    pyautogui.write('chrome')
+    pyautogui.press('enter')
+    sleep(1)
+    pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+    pyautogui.press('enter')
+    sleep(1)
 
-tabela = pandas.read_csv('produtos.csv')
+def fazer_login():
+    pyautogui.press('tab')
+    sleep(2)
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.write('joaovitorsimao13ff@gmail.com')
+    pyautogui.press('tab')
+    pyautogui.write('30220130Oa?')
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+    sleep(2)
 
-for linha in tabela.index:
+def importar_dados():
+    return pd.read_csv('produtos.csv')
+
+import time
+import pyautogui
+
+def cadastrar_produto(linha):
+  for linha in tabela.index:
     pyautogui.press('tab')
     pyautogui.write(str(tabela.loc[linha, 'codigo']))
     
@@ -54,6 +50,7 @@ for linha in tabela.index:
     pyautogui.press('tab')
     pyautogui.write(str(tabela.loc[linha, 'custo']))
     
+
     pyautogui.press('tab')
     obs = str(tabela.loc[linha, 'obs'])
     if obs != 'nan':
@@ -63,3 +60,17 @@ for linha in tabela.index:
     pyautogui.press('enter')
     pyautogui.click(x=1002, y=191)
 
+
+def cadastrar_todos_produtos(tabela):
+    for linha in tabela.itertuples(index=False):
+        cadastrar_produto(linha)
+
+def executar_processo():
+    acessar_sistema()  
+    fazer_login()     
+    tabela = importar_dados()  
+    cadastrar_todos_produtos(tabela)  
+
+if __name__ == '__main__':
+    pyautogui.PAUSE = 1.5 
+    executar_processo()
